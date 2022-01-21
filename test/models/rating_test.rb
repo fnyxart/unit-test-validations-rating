1d0a47d95d864e4ba3295cfa3830be11569f97bd
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2022-01-21 11:10:30 +0000

    Add TDD validations of score range

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -5,12 +5,24 @@ class RatingTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "valid if score is between 0 and 10" do
+  test 'valid if score is between 0 and 10' do
     0.upto(10).each do |i|
       rating = Rating.new(score: i)
       rating.valid?
       # Assert that the array of errors on our model for the score field is empty
       assert_empty rating.errors[:score]
     end
+  end
+
+  test 'invalid if score is less than 0' do
+    rating = Rating.new(score: -1)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
+  end
+
+  test 'invalid if score is greater than 10' do
+    rating = Rating.new(score: 11)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
   end
 end
