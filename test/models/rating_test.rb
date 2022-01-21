4158f1684f9e9c3c3dbb2b020f8bc239d2ea0b54
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2022-01-21 11:02:36 +0000

    Add AR valid. for rating + Fix users AR error

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -4,4 +4,13 @@ class RatingTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "valid if score is between 0 and 10" do
+    0.upto(10).each do |i|
+      rating = Rating.new(score: i)
+      rating.valid?
+      # Assert that the array of errors on our model for the score field is empty
+      assert_empty rating.errors[:score]
+    end
+  end
 end
